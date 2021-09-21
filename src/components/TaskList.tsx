11457a00a4915c3fd2b52eb82c498a087aba20dd
marcosparrotto <marcosparrotto@gmail.com>
2021-09-20 22:20:13 -0300

    teste

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,6 +16,7 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    //teste
   }
 
   function handleToggleTaskCompletion(id: number) {
